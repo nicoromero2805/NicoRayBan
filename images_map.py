98d7d5558f5f837520cf005b00547ebda5356764
nicoromero2805 <74071599+nicoromero2805@users.noreply.github.com>
2026-01-07 18:30:11 -0300

    Update images_map.py

--- images_map.py
+++ images_map.py
@@ -6,7 +6,7 @@ IMAGES = {
     ],
     "100103": [
         "https://drive.google.com/file/d/1N8eUiNyQVqU2jRpvUdT_5_bG6nwkhrm-/view?usp=drive_link",
-        "https://images2.ray-ban.com//cdn-record-files-pi/eef82e2e-44df-4fbb-9215-ad6b007db277/989a834c-7b64-403e-b45e-ad6b007db413/0RB4165__6597T5__STD__shad__qt.png?impolicy=RB_Product_clone&width=720&bgc=%23f2f2f2"
+        "https://images2.ray-ban.com//cdn-record-files-pi/eef82e2e-44df-4fbb-9215-ad6b007db277/989a834c-7b64-403e-b45e-ad6b007db413/0RB4165__6597T5__STD__shad__qt.png?impolicy=RB_Product_clone&width=720&bgc=%23f2f2f2",
     ],
     
     
