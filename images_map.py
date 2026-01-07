ed33117f93008dba9ece2a090aefa07067fabbe8
nicoromero2805 <74071599+nicoromero2805@users.noreply.github.com>
2026-01-07 17:25:59 -0300

    Update images_map.py

--- images_map.py
+++ images_map.py
@@ -2,9 +2,12 @@
 # Mapa simple SKU -> lista de URLs de imágenes
 IMAGES = {
     "100101": [
+        "https://drive.google.com/file/d/1p3UeGa1k0Y7613l5wZH_qOrwvH5VpYJa/view?usp=drive_link",
+        "https://drive.google.com/file/d/19fMhRsL_uiSfmhz4XmgYvesRo_76jkZx/view?usp=drive_link",
+        "https://drive.google.com/file/d/10oTAAvl_ZDTHEqN3dLmi6p1j9kZjthpV/view?usp=drive_link",
+    ],
+    "100610": [
+        "https://drive.google.com/file/d/1N8eUiNyQVqU2jRpvUdT_5_bG6nwkhrm-/view?usp=drive_link",
         "https://images2.ray-ban.com//cdn-record-files-pi/eef82e2e-44df-4fbb-9215-ad6b007db277/989a834c-7b64-403e-b45e-ad6b007db413/0RB4165__6597T5__STD__shad__qt.png?impolicy=RB_Product_clone&width=720&bgc=%23f2f2f2"
     ],
-    "100102": [
-        "https://images2.ray-ban.com//cdn-record-files-pi/5b7d0f4e-0b67-4b5d-9a33-5b3f3a1d5e2a/0RB3025__001_58__STD__shad__qt.png?impolicy=RB_Product_clone&width=720&bgc=%23f2f2f2"
-    ]
 }
