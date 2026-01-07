85b9e6cf63079f74ef1e7d7516e83e90e693f0d0
nicoromero2805 <74071599+nicoromero2805@users.noreply.github.com>
2026-01-07 18:51:10 -0300

    Update images_map.py

--- images_map.py
+++ images_map.py
@@ -2,11 +2,43 @@
 # Mapa simple SKU -> lista de URLs de imágenes
 IMAGES = {
     "100101": [
-        "https://drive.google.com/file/d/1p3UeGa1k0Y7613l5wZH_qOrwvH5VpYJa/view?usp=drive_link",
         "https://drive.google.com/file/d/19fMhRsL_uiSfmhz4XmgYvesRo_76jkZx/view?usp=drive_link",
+        "https://drive.google.com/file/d/10oTAAvl_ZDTHEqN3dLmi6p1j9kZjthpV/view?usp=drive_link",        
     ],
     "100103": [
-        "https://drive.google.com/file/d/1ZvGqYPw3UKmZElKloScOwfBq9xdiZWki/view?usp=drive_link",
         "https://drive.google.com/file/d/1PBfng2_NR4jqM9jnJwZmtm9ZnC3kz-mA/view?usp=drive_link",
-    ]
+        "https://drive.google.com/file/d/1_JPWF9geDLmzDYAX7lZlIZ_OeJOZ4Ma2/view?usp=drive_link",
+        "https://drive.google.com/file/d/1HYk0Hrocz8Xg4rAod4NOCnSsH2otIdo4/view?usp=drive_link",
+    ],
+    "100104": [
+        "https://drive.google.com/file/d/1zviky7DND5tTeyBWriaatFAwLYuuWkGO/view?usp=drive_link",
+        "https://drive.google.com/file/d/1NCdC09Y107Vu6tU4kOR18xXhMTh9EWlQ/view?usp=drive_link",
+        "https://drive.google.com/file/d/1s3xOxFQsmPTAAk3ywNjzwNoHgMfh5WbQ/view?usp=drive_link",        
+    ],
+    "100105": [
+        "https://drive.google.com/file/d/1zviky7DND5tTeyBWriaatFAwLYuuWkGO/view?usp=drive_link",
+        "https://drive.google.com/file/d/1NCdC09Y107Vu6tU4kOR18xXhMTh9EWlQ/view?usp=drive_link",
+        "https://drive.google.com/file/d/1s3xOxFQsmPTAAk3ywNjzwNoHgMfh5WbQ/view?usp=drive_link",
+    ],
+    "100106": [
+        "https://drive.google.com/file/d/1zviky7DND5tTeyBWriaatFAwLYuuWkGO/view?usp=drive_link",
+        "https://drive.google.com/file/d/1NCdC09Y107Vu6tU4kOR18xXhMTh9EWlQ/view?usp=drive_link",
+        "https://drive.google.com/file/d/1s3xOxFQsmPTAAk3ywNjzwNoHgMfh5WbQ/view?usp=drive_link",
+    ],
+     "100107": [
+        "https://drive.google.com/file/d/1_LyN6whzeKAGNkxMsXAxVgmixy28quYw/view?usp=drive_link",
+        "https://drive.google.com/file/d/1FLRJYmFHla5EYjzyqQHdIsYEj63ODj0y/view?usp=drive_link",
+        "https://drive.google.com/file/d/1RR4QxJYvIx2p01qWCk4WAS86OtKfjYL9/view?usp=drive_link",
+        
+    ], 
+    "100108": [
+        "https://drive.google.com/file/d/1_LyN6whzeKAGNkxMsXAxVgmixy28quYw/view?usp=drive_link",
+        "https://drive.google.com/file/d/1FLRJYmFHla5EYjzyqQHdIsYEj63ODj0y/view?usp=drive_link",
+        "https://drive.google.com/file/d/1RR4QxJYvIx2p01qWCk4WAS86OtKfjYL9/view?usp=drive_link",
+    ],
+    "100109": [
+        "https://drive.google.com/file/d/1_LyN6whzeKAGNkxMsXAxVgmixy28quYw/view?usp=drive_link",
+        "https://drive.google.com/file/d/1FLRJYmFHla5EYjzyqQHdIsYEj63ODj0y/view?usp=drive_link",
+        "https://drive.google.com/file/d/1RR4QxJYvIx2p01qWCk4WAS86OtKfjYL9/view?usp=drive_link",
+    ],
 }
