69d475d1e9cf058731a41c6c933208ac74a85476
nicoromero2805 <74071599+nicoromero2805@users.noreply.github.com>
2026-09-14 23:02:58 -0300

    Update Servicios.py

--- Servicios.py
+++ Servicios.py
@@ -40,7 +40,7 @@ def home():
 @app.get("/fecha")
 @app.get("/fecha/")
 def fecha():
-    return FileResponse("fecha/index2.html") # Comento por un archivo que no existe para que no vean el index original
+    return FileResponse("fecha/index.html") # Comento por un archivo que no existe para que no vean el index original
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
